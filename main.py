41e845c361af66287434dc297d062817135f23ae
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2025-08-17 13:28:42 +0700

    feat: membuat fungsi fibonaci menggunakan recursif fungsi

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # learning recursif
 # learning recursif to factorial
-from projek.recursif import fibonaciByForLoop
+from projek.recursif import *
 
 
 if __name__ == "__main__":
-    fibonaciByForLoop(5)
+    # fibonaciByForLoop(5)
+    # fibonaciByRecursif(1,0)
+    data = [F(i) for i in range(20)]
+    print(data)
--- projek/recursif.py
+++ projek/recursif.py
@@ -1,3 +1,6 @@
+# learning loop and recursif data in python
+
+# fibonaci by for loop
 def fibonaciByForLoop(n_range: int)->int:
     pref2 = 0
     pref1 = 1
@@ -6,3 +9,24 @@ def fibonaciByForLoop(n_range: int)->int:
         print("value to-> " + str(num_fibo))
         pref2 = pref1
         pref1 = num_fibo
+
+# fibonaci by recursif
+count = 2
+def fibonaciByRecursif(pref1: int, pref2: int)->int:
+    global count
+    if count <= 12:
+        num_fibonaci = pref1 + pref2
+        print(f"value to -> {num_fibonaci}")
+        pref2 = pref1
+        pref1 = num_fibonaci
+        count += 1
+        fibonaciByRecursif(pref1, pref2)
+    else:
+        return
+
+# menggunkan F(n) = F(n-1) + F(n-2)
+def F(n: int)->int:
+    if n <= 1:
+        return n
+    else:
+        return F(n-1) + F(n-2)
