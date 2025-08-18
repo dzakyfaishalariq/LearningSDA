0032bfe0e733536be769678e8831f4c12f0f5dc3
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2025-08-18 22:41:49 +0700

    feat: belajar selection sorting

--- main.py
+++ main.py
@@ -9,14 +9,25 @@ if __name__ == "__main__":
     # print(data)
 
     # print(min([9, 39, 10, 3, 4, 1, 3, 4, -1]))
-    start_time1 = time.time()
-    print(booble_sorting([3,1,4,2,4,6,7,4,2,9]))
+    # start_time1 = time.time()
+    # print(booble_sorting([3,1,4,2,4,6,7,4,2,9]))
+    # end_time1 = time.time()
+    # print("result eksekusi pogram 1 : " + str(end_time1-start_time1))
+    
+    # start_time2 = time.time()
+    # print(booble_sortingV2([3,1,4,2,4,6,7,4,2,9]))
+    # end_time2 = time.time()
+    # print("result eksekusi pogram 2 : " + str(end_time2-start_time2))
+    
+    star_time = time.time()
+    arry = selection_shorting([4,2,1,20,19,22,6,9])
+    print(arry)
+    end_time = time.time()
+    print(f"execution pogram : {end_time-star_time}")
+    
+
+    star_time1 = time.time()
+    arry1 = efisiensi_selection_shotrting([4,2,1,20,19,22,6,9])
+    print(arry1)
     end_time1 = time.time()
-    print("result eksekusi pogram 1 : " + str(end_time1-start_time1))
-    
-    start_time2 = time.time()
-    print(booble_sortingV2([3,1,4,2,4,6,7,4,2,9]))
-    end_time2 = time.time()
-    print("result eksekusi pogram 2 : " + str(end_time2-start_time2))
-    
-    
+    print(f"execution pogram : {end_time1-star_time1}")
--- projek/arrays.py
+++ projek/arrays.py
@@ -30,3 +30,25 @@ def booble_sortingV2(arr: list)->list:
         print(f"iterasi {i} : {arr}")
     return arr        
 
+# selection sort algorithem 
+def selection_shorting(arr: list)->list:
+    n = len(arr) # tentukan panjang lisnya adalah 8
+    for i in range(n): # lakukan perulangan dari index 0 pada array dengan rentan 8-1 = 7 
+        index_min = i # jadikan index min adalah nilai i = 0
+        for j in range(i + 1, n): # lakukan perulangan j = 1 pada array dengan rentang (1,8)
+            if arr[j] < arr[index_min]: # lakukan pengecekan secara berkala dari array[1] lebih kecil dari array[0]
+                index_min=j # ubah nilai index_minimum menjadi nilai j = 1
+        value_min = arr.pop(index_min) # ambil nilai minimum terakhir dengan menghapus nilai lis sebelumnya pada index_min yang berubah
+        arr.insert(i, value_min)
+    return arr
+
+def efisiensi_selection_shotrting(arr: list)->list:
+    n = len(arr)
+    for i in range(n):
+        index_min = i
+        for j in range(i+1, n):
+            if arr[j] < arr[index_min]:
+                index_min = j
+        arr[i], arr[index_min] = arr[index_min], arr[i]
+    return arr
+
